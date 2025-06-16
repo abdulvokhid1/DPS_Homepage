6c6dd1b57fecaaa98477df8051446f9886e4da99
굳프렌즈 <ibrakhimov7747a@gmail.com>
2025-06-16 16:14:09 +0900

    Fix: corrected Multer types for Express

--- src/libs/types/types/member.ts
+++ src/libs/types/types/member.ts
@@ -1,64 +1,63 @@
+/** @format */
+
 import { ObjectId } from "mongoose";
 import { Session } from "express-session";
 import { Request } from "express";
+import { Multer } from "multer";
 import { MemberStatus, MemberType } from "../../enums/member.enum";
 
-export interface Member{
-    _id: ObjectId;
-    memberType: MemberType;
-    memberStatus: MemberStatus;
-    memberNick: string;
-    memberPhone:string; 
-    memberPassword?:string;
-    memberAddress?:string;
-    memberDesk?:string;
-    memberImage?:string;
-    memberPoints:number;
-    createdAt:Date;
-    updatedAt:Date;
-
+export interface Member {
+  _id: ObjectId;
+  memberType: MemberType;
+  memberStatus: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword?: string;
+  memberAddress?: string;
+  memberDesk?: string;
+  memberImage?: string;
+  memberPoints: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-
-export interface MemberInput{
-    memberType?: MemberType;
-    memberStatus?: MemberStatus;
-    memberNick: string;
-    memberPhone:string;
-    memberPassword:string;
-    memberAddress?:string;
-    memberDesk?:string;
-    memberImage?:string;
-    memberPoints?:number;
-
+export interface MemberInput {
+  memberType?: MemberType;
+  memberStatus?: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword: string;
+  memberAddress?: string;
+  memberDesk?: string;
+  memberImage?: string;
+  memberPoints?: number;
 }
 
-export interface LoginInput{
-    memberNick:string;
-    memberPassword:string;
+export interface LoginInput {
+  memberNick: string;
+  memberPassword: string;
 }
 
-export interface MemberUpdateInput{
-    _id: ObjectId;
-    memberStatus?: MemberStatus;
-    memberNick?: string;
-    memberPhone?:string;
-    memberPassword?:string;
-    memberAddress?:string;
-    memberDesk?:string;
-    memberImage?:string;
-
+export interface MemberUpdateInput {
+  _id: ObjectId;
+  memberStatus?: MemberStatus;
+  memberNick?: string;
+  memberPhone?: string;
+  memberPassword?: string;
+  memberAddress?: string;
+  memberDesk?: string;
+  memberImage?: string;
 }
 
 export interface ExtendedRequest extends Request {
-    member:Member;
-    file: Express.Multer.File;
-    files: Express.Multer.File[];
+  member: Member;
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
 }
 
 export interface AdminRequest extends Request {
-    member:Member;
-    session: Session & {member: Member}
-    file: Express.Multer.File;
-    files: Express.Multer.File[];
+  member: Member;
+  session: Session & { member: Member };
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
 }
